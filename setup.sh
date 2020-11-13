6bd73176f7bfba2b01bccfb9aefb9b1047ccf8c6
Mike Wallio <michael.wallio@outlook.com>
2020-11-13 10:31:04 -0500

    Add PackageKit-command-not-found
    PackageKit-command-not-found is normally installed by default in Fedora but not the docker cloud image.
    
    To have a more normal experience I think setting it up is desired.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 dnf update
-dnf install wget curl sudo ncurses dnf-plugins-core dnf-utils passwd findutils cracklib-dicts glibc-locale-source glibc-langpack-en procps-ng util-linux-user 
+dnf install wget curl sudo ncurses dnf-plugins-core dnf-utils passwd findutils cracklib-dicts glibc-locale-source glibc-langpack-en procps-ng util-linux-user PackageKit-command-not-found
 dnf group install "Development Tools"
 
 echo $1
