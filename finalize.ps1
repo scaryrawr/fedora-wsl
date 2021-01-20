ff28403df0f124fe6c8bd9ced0cbdc08aaeba7b5
Mike Wallio <miwalli@microsoft.com>
2021-01-20 17:31:14 -0500

    Add rawhide(34) branch

--- finalize.ps1
+++ finalize.ps1
@@ -1,2 +1,2 @@
 # Set default user as first non-root user
-Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq Fedora  | Set-ItemProperty -Name DefaultUid -Value 1000
+Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq Rawhide  | Set-ItemProperty -Name DefaultUid -Value 1000
--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,14 @@
 # Download Cloud Image
-Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/33/x86_64/fedora-33.20201110-x86_64.tar.xz -OutFile Fedora.tar.xz
+Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/34/x86_64/fedora-Rawhide.20201230-x86_64.tar.xz -OutFile Fedora.tar.xz
 
 # Extract
 7z e .\Fedora.tar.xz
 
 # Make distro folder
-mkdir $HOME\Distros\Fedora
+mkdir "$HOME\Distros\Fedora (Rawhide)"
 
 # Import cloud image
-wsl --import Fedora $HOME\Distros\Fedora .\Fedora.tar
+wsl --import Rawhide "$HOME\Distros\Fedora (Rawhide)" .\Fedora.tar
 
 # Launch to configure
-wsl -d Fedora
+wsl -d Rawhide
