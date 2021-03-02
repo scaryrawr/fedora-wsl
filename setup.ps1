14a281cda93ebd0ca439f6d56c54bb1caea2227c
Mike Wallio <michael.wallio@outlook.com>
2021-03-02 16:41:38 -0500

    Update build version

--- setup.ps1
+++ setup.ps1
@@ -1,11 +1,13 @@
 # Download Cloud Image
-Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/33/x86_64/fedora-33.20201110-x86_64.tar.xz -OutFile Fedora.tar.xz
+Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/33/x86_64/fedora-33.20210217-x86_64.tar.xz -OutFile Fedora.tar.xz
 
 # Extract
 7z e .\Fedora.tar.xz
 
 # Make distro folder
-mkdir $HOME\Distros\Fedora
+if (!(Test-Path "$HOME\Distros\Fedora")) {
+    mkdir $HOME\Distros\Fedora
+}
 
 # Import cloud image
 wsl --import Fedora $HOME\Distros\Fedora .\Fedora.tar
