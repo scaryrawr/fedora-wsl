dafe35cd23b27ef3f9c14be91ea36ed6aac5e45a
Mike Wallio <michael.wallio@outlook.com>
2021-04-08 20:22:42 -0400

    Update to app data

--- setup.ps1
+++ setup.ps1
@@ -1,16 +1,16 @@
 # Download Cloud Image
-Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/33/x86_64/fedora-33.20210217-x86_64.tar.xz -OutFile Fedora.tar.xz
+Invoke-WebRequest -Uri https://github.com/fedora-cloud/docker-brew-fedora/raw/33/x86_64/fedora-33.20210401-x86_64.tar.xz -OutFile Fedora.tar.xz
 
 # Extract
 7z e .\Fedora.tar.xz
 
 # Make distro folder
-if (!(Test-Path "$HOME\Distros\Fedora")) {
-    mkdir $HOME\Distros\Fedora
+if (!(Test-Path "$HOME\AppData\Local\Fedora")) {
+    mkdir $HOME\AppData\Local\Fedora
 }
 
 # Import cloud image
-wsl --import Fedora $HOME\Distros\Fedora .\Fedora.tar
+wsl --import Fedora $HOME\AppData\Local\Fedora .\Fedora.tar
 
 # Launch to configure
 wsl -d Fedora
