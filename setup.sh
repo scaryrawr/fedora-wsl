18eb510b8dc547bdae5c60de09da82a65bb92408
Mike Wallio <michael.wallio@outlook.com>
2021-07-30 13:11:50 -0700

    Try to get WSLg in a better state

--- setup.sh
+++ setup.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env sh
 
 dnf update -y
-dnf install -y wget curl sudo ncurses dnf-plugins-core dnf-utils passwd findutils cracklib-dicts glibc-locale-source glibc-langpack-en which
+
+# Install packages required for a more "expected" experience
+dnf install -y wget curl sudo ncurses dnf-plugins-core dnf-utils passwd findutils cracklib-dicts glibc-locale-source glibc-langpack-en which git-lfs vim
+
+# Get developer tools
 dnf groupinstall -y 'Development Libraries' 'Development Tools'
+
+# RPM Fusion
+dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf groupupdate -y core
+dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+dnf groupupdate -y sound-and-video
+
+# Install Mesa drivers and libraries for WSLg
+dnf install -y mesa-dri-drivers mesa-libEGL mesa-libOpenCL mesa-libGLU mesa-libGL mesa-libOSMesa mesa-li
+bd3d mesa-libgbm mesa-libglapi mesa-libxatracker mesa-omx-drivers mesa-vdpau-drivers mesa-vulkan-drivers
 
 username=$1
 
