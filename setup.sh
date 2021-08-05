c3fe19351fadc55106e3a78ace24c3408561f0ad
Mike Wallio <michael.wallio@outlook.com>
2021-08-04 21:58:46 -0700

    Build mesa with WSLg support

--- setup.sh
+++ setup.sh
@@ -14,8 +14,19 @@ dnf groupupdate -y core
 dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 dnf groupupdate -y sound-and-video
 
-# Install Mesa drivers and libraries for WSLg
-dnf install -y mesa-dri-drivers mesa-libEGL mesa-libOpenCL mesa-libGLU mesa-libGL mesa-libOSMesa mesa-libd3d mesa-libgbm mesa-libglapi mesa-libxatracker mesa-omx-drivers mesa-vdpau-drivers mesa-vulkan-drivers
+# Build mesa with WSLg support
+dnf builddep -y mesa
+
+cd ~
+git clone --depth 1 https://github.com/mesa3d/mesa
+cd mesa
+mkdir build && cd build
+meson .. -Dprefix=/usr -Ddri-drivers= -Dgallium-drivers=swrast,d3d12 -Dbuildtype=release
+sudo ninja install
+cd ~
+rm -rf mesa
+
+dnf install -y glx-utils
 
 # Remove "Container Image" strings so neofetch looks "cooler"
 sed -i 's/ (Container Image)//g' /etc/os-release
