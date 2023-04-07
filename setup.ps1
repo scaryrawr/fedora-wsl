acbca63a90145a310a620e2f05af074fca556f3f
Mike Wallio <michael.wallio@outlook.com>
2023-04-07 17:58:32 -0400

    Update for bottle updates

--- setup.ps1
+++ setup.ps1
@@ -42,7 +42,6 @@ if (!(Test-Path "$InstallDirectory\$DistroName")) {
 
 # Import cloud image
 wsl --import "$DistroName" "$InstallDirectory\$DistroName" "$scriptDir\Fedora-${Version}.tar"
-$wslVersion = (Get-AppxPackage -name 'MicrosoftCorporationII.WindowsSubsystemForLinux').Version
 
 $nixPath = wsl -e wslpath "$scriptDir"
 
@@ -52,55 +51,8 @@ wsl -d "$DistroName" "$nixPath/setup.sh" $UserName
 # Set default user as first non-root user
 Get-ItemProperty Registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss\*\ DistributionName | Where-Object -Property DistributionName -eq "$DistroName"  | Set-ItemProperty -Name DefaultUid -Value 1000
 
-# Version 0.67.6.0 and newer support systemd by default
-if ($wslVersion -lt '0.67.6.0') {
-    wsl -u root -d "$DistroName" "$nixPath/systemd-setup.sh"
-
-    # Update Windows Terminal Profiles
-    $guid = (New-Guid).Guid.ToString()
-    @(
-        # Stable
-        "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState", 
-
-        # Preview
-        "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState"
-    ) | ForEach-Object {
-        $terminalDir = "$_"
-        $terminalProfile = "$terminalDir/settings.json"
-
-        # This version of windows terminal isn't installed
-        if (!(Test-Path $terminalProfile)) {
-            return
-        }
-
-        # Load existing profile
-        $configData = (Get-Content -Path $terminalProfile | ConvertFrom-Json) | Where-Object { $_ -ne $null }
-
-        # Create a new list to store profiles
-        $profiles = New-Object Collections.Generic.List[Object]
-
-        $configData.profiles.list | Where-Object { $_.name -ne "${DistroName}" } | ForEach-Object { $profiles.Add($_) }
-        $profiles.Add(@{
-                commandline = "C:\WINDOWS\system32\wsl.exe -d ${DistroName} --cd ~ -e /usr/libexec/nslogin /bin/bash"
-                name        = "${DistroName}"
-                hidden      = $false
-                guid        = "{$guid}"
-                icon        = 'ms-appx:///ProfileIcons/{9acb9455-ca41-5af7-950f-6bca1bc9722f}.png'
-            })
-    
-        # Update color schemes
-        $configData.profiles.list = $profiles
-
-        # Write config to disk
-        $configData | ConvertTo-Json -Depth 32 | Set-Content -Path $terminalProfile
-    }
-}
-else {
-    wsl -u root -d "$DistroName" "$nixPath/systemd-enable.sh"
-}
-
+wsl -u root -d "$DistroName" "$nixPath/systemd-enable.sh"
 wsl -u root -d "$DistroName" "$nixPath/setup-user-runtime.sh"
-wsl -u root -d "$DistroName" "$nixPath/setup-xwayland.sh"
 
 # Terminate for launching with systemd support
 wsl -t "${DistroName}"
