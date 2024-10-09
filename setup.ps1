aa8aa360d7f894f1d88c015ea3d9021500668e83
Mike Wallio <michael.wallio@outlook.com>
2024-10-09 10:45:40 -0400

    Update to use scaryrawr/Fedora-WSL-RootFS

--- setup.ps1
+++ setup.ps1
@@ -7,41 +7,26 @@ param(
     $DistroName = 'Fedora',
     # Install Location
     [string]
-    $InstallDirectory = "$HOME\AppData\Local\",
-    # Fedora version to install
-    [string]
-    [ValidateScript(
-        { $_ -in (((Invoke-WebRequest -Uri https://api.github.com/repos/fedora-cloud/docker-brew-fedora/branches).Content | ConvertFrom-Json).name | Where-Object { $_ -match "^\d+$" }) },
-        ErrorMessage = 'Invalid Fedora Version, please check available branches https://github.com/fedora-cloud/docker-brew-fedora/branches'
-    )]
-    $Version = "39"
+    $InstallDirectory = "$HOME\AppData\Local\"
 )
 
 # Determine ARM64 or x86_64
-$arch = 'x86_64'
+$arch = 'amd64'
 if ("$env:PROCESSOR_ARCHITECTURE" -eq 'ARM64') {
-    $arch = 'aarch64'
+    $arch = 'arm64'
 }
 
 $scriptDir = Split-Path $PSCommandPath
 
-# Download Cloud Image
-if (!(Test-Path "$scriptDir\Fedora-${Version}.tar.xz")) {
-    Invoke-WebRequest -Uri "https://github.com/fedora-cloud/docker-brew-fedora/raw/${Version}/${arch}/fedora-${Version}-${arch}.tar.xz" -OutFile "$scriptDir\Fedora-${Version}.tar.xz" -ErrorAction Stop
-}
-
-# Extract
-if (!(Test-Path "$scriptDir\Fedora-${Version}.tar")) {
-    7z e "$scriptDir\Fedora-${Version}.tar.xz" -o"$scriptDir\"
-}
+Invoke-WebRequest -Uri "https://github.com/scaryrawr/Fedora-WSL-RootFS/releases/latest/download/rootfs.${arch}.tar.gz" -OutFile "$scriptDir\Fedora${Version}.tar.gz" -ErrorAction Stop
 
 # Make distro folder
 if (!(Test-Path "$InstallDirectory\$DistroName")) {
     mkdir "$InstallDirectory\$DistroName"
 }
 
-# Import cloud image
-wsl --import "$DistroName" "$InstallDirectory\$DistroName" "$scriptDir\Fedora-${Version}.tar"
+# Import image
+wsl --import "$DistroName" "$InstallDirectory\$DistroName" "$scriptDir\Fedora${Version}.tar.gz"
 
 $nixPath = wsl -e wslpath "$scriptDir"
 
--- setup.sh
+++ setup.sh
@@ -1,37 +1,5 @@
 #!/usr/bin/env sh
 
-# Remove no docs flag
-sed -i '/nodocs/d' /etc/dnf/dnf.conf
-
-# Enable systemd
-echo '[boot]
-systemd=true' >> /etc/wsl.conf
-
-# Install packages required for a more "expected" experience
-dnf install -y wget curl sudo ncurses dnf-plugins-core dnf-utils passwd findutils cracklib-dicts glibc-locale-source glibc-langpack-en which git-lfs vim PackageKit-command-not-found
-
-# Get developer tools
-dnf groupinstall -y 'Development Libraries' 'Development Tools'
-
-# RPM Fusion
-dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf groupupdate -y core
-dnf groupupdate -y multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
-dnf groupupdate -y sound-and-video
-dnf copr enable scaryrawr/mesa-d3d12 -y
-dnf update -y
-dnf install mesa-dri-drivers mesa-d3d12 mesa-vdpau-drivers -y
-
-git clone https://github.com/scaryrawr/bottle-imp
-
-cd bottle-imp
-
-make internal-systemd
-make internal-binfmt
-
-cd ..
-sudo rm -rf bottle-imp
-
 username=$1
 
 if [ -z "$username" ]; then
